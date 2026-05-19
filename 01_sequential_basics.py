0862895fd824ff68e362cb4c6131c0c1ba4fa664
JGM-droid <jessegmontemayor@gmail.com>
2026-05-19 13:37:26 -0500

    Build first Sequential neural network architecture

--- 01_sequential_basics.py
+++ 01_sequential_basics.py
@@ -5,3 +5,18 @@ from tensorflow.keras.layers import Dense
 import numpy as np
 
 print("imports successful")
+
+#create a sequential neural network model
+model=Sequential()
+
+print("sequential model created")
+
+#hidden layer with 3 neurons and relu activation function
+model.add(Dense(units=3, activation='relu', input_shape=(3,)))
+
+print("hidden layer added")
+
+# Output layer with 1 neuron
+model.add(Dense(units=1))
+
+print("output layer added")
