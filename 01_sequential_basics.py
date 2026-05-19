a2dfd388d24c0da78b1b39519725f59cfbf77e66
JGM-droid <jessegmontemayor@gmail.com>
2026-05-19 13:09:49 -0500

    Set up TensorFlow environment and verify Keras imports

---
+++ 01_sequential_basics.py
@@ -0,0 +1,7 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+print("imports successful")
